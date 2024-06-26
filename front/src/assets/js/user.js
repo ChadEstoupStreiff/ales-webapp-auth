cdef8f06691e0c164739452f44cb9d40beb501e2
Elouan Lahellec <elouan.lahellec@gmail.com>
2024-06-26 17:14:42 +0200

    correction affichage date

--- front/src/assets/js/user.js
+++ front/src/assets/js/user.js
@@ -6,16 +6,16 @@ function fetchUserInfo(token) {
             'Authorization': `Bearer ${token}`
         }
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(userInfo => {
-        var date = new Date(userInfo.exp);
-        document.getElementById('userInfo').innerHTML = "<h3>ID: " + userInfo.sub + "</h3><h3>Name: " + userInfo.name + "</h3><h3>E-Mail: " + userInfo.email + "</h3><h3>Expiration: " + date.toUTCString() + "</h3>";
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(userInfo => {
+            var date = new Date(userInfo.exp * 1000);
+            document.getElementById('userInfo').innerHTML = "<h3>ID: " + userInfo.sub + "</h3><h3>Name: " + userInfo.name + "</h3><h3>E-Mail: " + userInfo.email + "</h3><h3>Expiration: " + date.toUTCString() + "</h3>";
+        });
 }
 
 token = loadToken()
@@ -31,38 +31,38 @@ function reload() {
             'Authorization': `Bearer ${token}`
         },
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(flights => {   
-        html = ""
-        flights.forEach(e => {
-            html = html + "<hr/><div class='flighinfo'><p>ID: " + e.flight_id + "</p><p>Departure: " + e.departure_date + "</p><p>Return: " + e.return_date + "</p><p>Nbr of tickets: " + e.number_of_tickets + "</p></div>"
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(flights => {
+            html = ""
+            flights.forEach(e => {
+                html = html + "<hr/><div class='flighinfo'><p>ID: " + e.flight_id + "</p><p>Departure: " + e.departure_date + "</p><p>Return: " + e.return_date + "</p><p>Nbr of tickets: " + e.number_of_tickets + "</p></div>"
+            });
+            document.getElementById("flights_list").innerHTML = html
         });
-        document.getElementById("flights_list").innerHTML = html
-    });
 
     fetch('http://localhost:8902/hotels/reservations', {
         headers: {
             'Authorization': `Bearer ${token}`
         },
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(hotels => {
-        html = ""
-        hotels.forEach(e => {
-            html = html + "<hr/><div class='hotelinfo'><p>ID: " + e.hotel_id + "</p><p>Check in: " + e.check_in_date + "</p><p>Check out: " + e.check_out_date + "</p><p>Nbr of rooms: " + e.number_of_rooms + "</p></div>"
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(hotels => {
+            html = ""
+            hotels.forEach(e => {
+                html = html + "<hr/><div class='hotelinfo'><p>ID: " + e.hotel_id + "</p><p>Check in: " + e.check_in_date + "</p><p>Check out: " + e.check_out_date + "</p><p>Nbr of rooms: " + e.number_of_rooms + "</p></div>"
+            });
+            document.getElementById("hotels_list").innerHTML = html
         });
-        document.getElementById("hotels_list").innerHTML = html
-    });
 }
 
 document.getElementById('flight_reserve').addEventListener('click', () => {
@@ -72,15 +72,15 @@ document.getElementById('flight_reserve').addEventListener('click', () => {
         },
         method: "POST",
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(_ => {
-        reload()  
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(_ => {
+            reload()
+        });
 });
 
 
@@ -91,13 +91,13 @@ document.getElementById('hotel_reserve').addEventListener('click', () => {
         },
         method: "POST",
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(_ => {
-        reload()  
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(_ => {
+            reload()
+        });
 });
